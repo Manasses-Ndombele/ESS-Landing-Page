53937cae5ea3a0de71211da75b90c4b4a8141bcc
Manassés Ndombele <manassesndombelefreelancer@gmail.com>
2025-01-27 10:33:17 +0000

    Terminei a versão de tablet de todos os componentes

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -12,9 +12,11 @@ function MainHeader() {
                     <path fill="#FB1313" fillOpacity="1" d="M0,32L80,58.7C160,85,320,139,480,144C640,149,800,107,960,90.7C1120,75,1280,85,1360,90.7L1440,96L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path>
                 </svg>
             </div>
-            <BrandTitle />
-            <h2 className="slogan text-center p-3 fw-bold text-uppercase">Passo a passo infalível para ser uma pessoa mais <strong>saudável e fitness!</strong></h2>
-            <p className="intro text-center p-3">Saiba como milhares de utilizaram o método secreto e inusitado do nosso produto <strong>Emagrecimento Sem Segredos</strong> e alcance você também o seu sonho através deste curso incrível!</p>
+            <div className="intro-area">
+                <BrandTitle />
+                <h2 className="slogan text-center p-3 fw-bold text-uppercase">Passo a passo infalível para ser uma pessoa mais <strong>saudável e fitness!</strong></h2>
+                <p className="intro text-center p-3">Saiba como milhares utilizaram o método secreto e inusitado do nosso produto <strong>Emagrecimento Sem Segredos</strong> e alcance você também o seu sonho através deste curso incrível!</p>
+            </div>
         </header>
     )
 }
--- src/components/TestimoniesArea.jsx
+++ src/components/TestimoniesArea.jsx
@@ -7,47 +7,59 @@ function TestimoniesArea() {
             <h2 className="text-center my-3">TESTEMUNHOS DE ALGUNS ALUNOS!</h2>
             <div id="testimonies-carousel" className="carousel slide carousel-fade mb-4" data-bs-ride="carousel">
                 <div className="carousel-inner testimonies-containers">
-                    <div className="testimony-container carousel-item active rounded-3 p-3" data-bs-interval="3000">
-                        <p className="testimony-header d-flex align-items-center gap-1 fw-bold">
-                            <img src="/testimony-1.webp" alt="Foto de um cliente satisfeito" className="rounded-circle p-2" />
-                            <span>Bruno Peregrino</span>
-                        </p>
-                        <p>“Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci voluptatibus ab beatae eum nulla explicabo repudiandae quis. At quos illum, eveniet veritatis quibusdam illo accusantium adipisci reprehenderit fugit sapiente nulla!”</p>
+                    <div className="carousel-item active" data-bs-interval="3000">
+                        <div className="testimony-container rounded-3 p-3">
+                            <p className="testimony-header d-flex align-items-center gap-1 fw-bold">
+                                <img src="/testimony-1.webp" alt="Foto de um cliente satisfeito" className="rounded-circle p-2" />
+                                <span>Bruno Peregrino</span>
+                            </p>
+                            <p>“Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci voluptatibus ab beatae eum nulla explicabo repudiandae quis. At quos illum, eveniet veritatis quibusdam illo accusantium adipisci reprehenderit fugit sapiente nulla!”</p>
+                        </div>
                     </div>
-                    <div className="testimony-container carousel-item rounded-3 p-3" data-bs-interval="3000">
-                        <p className="testimony-header d-flex align-items-center gap-1 fw-bold">
-                            <img src="/testimony-2.webp" alt="Foto de um cliente satisfeito" className="rounded-circle p-2" />
-                            <span>Malu Peregrina</span>
-                        </p>
-                        <p>“Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci voluptatibus ab beatae eum nulla explicabo repudiandae quis. At quos illum, eveniet veritatis quibusdam illo accusantium adipisci reprehenderit fugit sapiente nulla!”</p>
+                    <div className="carousel-item" data-bs-interval="3000">
+                        <div className="testimony-container rounded-3 p-3">
+                            <p className="testimony-header d-flex align-items-center gap-1 fw-bold">
+                                <img src="/testimony-2.webp" alt="Foto de um cliente satisfeito" className="rounded-circle p-2" />
+                                <span>Malu Peregrina</span>
+                            </p>
+                            <p>“Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci voluptatibus ab beatae eum nulla explicabo repudiandae quis. At quos illum, eveniet veritatis quibusdam illo accusantium adipisci reprehenderit fugit sapiente nulla!”</p>
+                        </div>
                     </div>
-                    <div className="testimony-container carousel-item rounded-3 p-3" data-bs-interval="3000">
-                        <p className="testimony-header d-flex align-items-center gap-1 fw-bold">
-                            <img src="/testimony-3.webp" alt="Foto de um cliente satisfeito" className="rounded-circle p-2" />
-                            <span>Thiago Nicéia</span>
-                        </p>
-                        <p>“Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci voluptatibus ab beatae eum nulla explicabo repudiandae quis. At quos illum, eveniet veritatis quibusdam illo accusantium adipisci reprehenderit fugit sapiente nulla!”</p>
+                    <div className="carousel-item" data-bs-interval="3000">
+                        <div className="testimony-container rounded-3 p-3">
+                            <p className="testimony-header d-flex align-items-center gap-1 fw-bold">
+                                <img src="/testimony-3.webp" alt="Foto de um cliente satisfeito" className="rounded-circle p-2" />
+                                <span>Thiago Nicéia</span>
+                            </p>
+                            <p>“Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci voluptatibus ab beatae eum nulla explicabo repudiandae quis. At quos illum, eveniet veritatis quibusdam illo accusantium adipisci reprehenderit fugit sapiente nulla!”</p>
+                        </div>
                     </div>
-                    <div className="testimony-container carousel-item rounded-3 p-3" data-bs-interval="3000">
-                        <p className="testimony-header d-flex align-items-center gap-1 fw-bold">
-                            <img src="/testimony-4.jpg" alt="Foto de um cliente satisfeito" className="rounded-circle p-2" />
-                            <span>Thiago Finéas</span>
-                        </p>
-                        <p>“Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci voluptatibus ab beatae eum nulla explicabo repudiandae quis. At quos illum, eveniet veritatis quibusdam illo accusantium adipisci reprehenderit fugit sapiente nulla!”</p>
+                    <div className="carousel-item" data-bs-interval="3000">
+                        <div className="testimony-container rounded-3 p-3">
+                            <p className="testimony-header d-flex align-items-center gap-1 fw-bold">
+                                <img src="/testimony-4.jpg" alt="Foto de um cliente satisfeito" className="rounded-circle p-2" />
+                                <span>Thiago Finéas</span>
+                            </p>
+                            <p>“Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci voluptatibus ab beatae eum nulla explicabo repudiandae quis. At quos illum, eveniet veritatis quibusdam illo accusantium adipisci reprehenderit fugit sapiente nulla!”</p>
+                        </div>
                     </div>
-                    <div className="testimony-container carousel-item rounded-3 p-3" data-bs-interval="3000">
-                        <p className="testimony-header d-flex align-items-center gap-1 fw-bold">
-                            <img src="/testimony-5.webp" alt="Foto de um cliente satisfeito" className="rounded-circle p-2" />
-                            <span>Pablo Marcelo</span>
-                        </p>
-                        <p>“Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci voluptatibus ab beatae eum nulla explicabo repudiandae quis. At quos illum, eveniet veritatis quibusdam illo accusantium adipisci reprehenderit fugit sapiente nulla!”</p>
+                    <div className="carousel-item" data-bs-interval="3000">
+                        <div className="testimony-container rounded-3 p-3">
+                            <p className="testimony-header d-flex align-items-center gap-1 fw-bold">
+                                <img src="/testimony-5.webp" alt="Foto de um cliente satisfeito" className="rounded-circle p-2" />
+                                <span>Pablo Marcelo</span>
+                            </p>
+                            <p>“Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci voluptatibus ab beatae eum nulla explicabo repudiandae quis. At quos illum, eveniet veritatis quibusdam illo accusantium adipisci reprehenderit fugit sapiente nulla!”</p>
+                        </div>
                     </div>
-                    <div className="testimony-container carousel-item rounded-3 p-3" data-bs-interval="3000">
-                        <p className="testimony-header d-flex align-items-center gap-1 fw-bold">
-                            <img src="/testimony-6.webp" alt="Foto de um cliente satisfeito" className="rounded-circle p-2" />
-                            <span>Bruno Perrucho</span>
-                        </p>
-                        <p>“Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci voluptatibus ab beatae eum nulla explicabo repudiandae quis. At quos illum, eveniet veritatis quibusdam illo accusantium adipisci reprehenderit fugit sapiente nulla!”</p>
+                    <div className="carousel-item" data-bs-interval="3000">
+                        <div className="testimony-container rounded-3 p-3">
+                            <p className="testimony-header d-flex align-items-center gap-1 fw-bold">
+                                <img src="/testimony-6.webp" alt="Foto de um cliente satisfeito" className="rounded-circle p-2" />
+                                <span>Bruno Perrucho</span>
+                            </p>
+                            <p>“Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci voluptatibus ab beatae eum nulla explicabo repudiandae quis. At quos illum, eveniet veritatis quibusdam illo accusantium adipisci reprehenderit fugit sapiente nulla!”</p>
+                        </div>
                     </div>
                 </div>
                 <button className="carousel-control-prev" type="button" data-bs-target="#testimonies-carousel" data-bs-slide="prev">
@@ -59,7 +71,9 @@ function TestimoniesArea() {
                     <span className="visually-hidden">Next</span>
                 </button>
             </div>
-            <MainCtaBtn text="QUERO EMAGRECER AGORA" />
+            <div className="cta-container">
+                <MainCtaBtn text="QUERO EMAGRECER AGORA" />
+            </div>
         </div>
     )
 }
